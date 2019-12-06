702f5ca8238175ea74db7b7d2fc4916e4d6424ff
Angot <bat76640@gmail.com>
2019-12-06 10:28:22 +0100

    cluster

--- wp-content/themes/benzai/template-map.php
+++ wp-content/themes/benzai/template-map.php
@@ -37,53 +37,118 @@
         zoom: 3
     });
 
-    request.features.forEach(function (marker) {
-        var el = document.createElement(("div"));
+    map.on('load', function() {
+// Add a new source from our GeoJSON data and set the
+// 'cluster' option to true. GL-JS will add the point_count property to your source data.
+        map.addSource("BIN", {
+            type: "geojson",
+// Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+// from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+            data: request,
+            cluster: true,
+            clusterMaxZoom: 14, // Max zoom to cluster points on
+            clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+        });
 
-        el.className = 'marker';
-        el.style.backgroundImage = 'url(http://download.seaicons.com/icons/cornmanthe3rd/plex/32/System-recycling-bin-full-icon.png)';
-        el.style.width = '32px';
-        el.style.height = '32px';
+        // add markers to map
+        //request.features.forEach(function(marker) {
 
-        var popup = new mapboxgl.Popup({offset: 25})
-            .setText(marker.properties.adresse);
+        // create a HTML element for each feature
+        //var el = document.createElement('div');
+        //el.className = 'marker';
 
-        new mapboxgl.Marker(el)
-            .setLngLat(marker.geometry.coordinates)
-            .setPopup(popup)
-            .addTo(map);
+        // make a marker for each feature and add to the map
+        //new mapboxgl.Marker(el)
+        //.setLngLat(marker.geometry.coordinates)
+        //.addTo(map);
+        //});
+
+        map.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        }));
+
+        map.addLayer({
+            id: "clusters",
+            type: "circle",
+            source: "BIN",
+            filter: ["has", "point_count"],
+            paint: {
+// Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+// with three steps to implement three types of circles:
+//   * Blue, 20px circles when point count is less than 100
+//   * Yellow, 30px circles when point count is between 100 and 750
+//   * Pink, 40px circles when point count is greater than or equal to 750
+                "circle-color": [
+                    "step",
+                    ["get", "point_count"],
+                    "#51bbd6",
+                    100,
+                    "#f1f075",
+                    750,
+                    "#f28cb1"
+                ],
+                "circle-radius": [
+                    "step",
+                    ["get", "point_count"],
+                    20,
+                    100,
+                    30,
+                    750,
+                    40
+                ]
+            }
+        });
+
+        map.addLayer({
+            id: "cluster-count",
+            type: "symbol",
+            source: "BIN",
+            filter: ["has", "point_count"],
+            layout: {
+                "text-field": "{point_count_abbreviated}",
+                "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+                "text-size": 12
+            }
+        });
+
+        map.addLayer({
+            id: "unclustered-point",
+            type: "circle",
+            source: "BIN",
+            filter: ["!", ["has", "point_count"]],
+            paint: {
+                "circle-color": "#11b4da",
+                "circle-radius": 4,
+                "circle-stroke-width": 1,
+                "circle-stroke-color": "#fff"
+            }
+        });
+
+// inspect a cluster on click
+        map.on('click', 'clusters', function (e) {
+            var features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
+            var clusterId = features[0].properties.cluster_id;
+            map.getSource('BIN').getClusterExpansionZoom(clusterId, function (err, zoom) {
+                if (err)
+                    return;
+
+                map.easeTo({
+                    center: features[0].geometry.coordinates,
+                    zoom: zoom
+                });
+            });
+        });
+
+        map.on('mouseenter', 'clusters', function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+        map.on('mouseleave', 'clusters', function () {
+            map.getCanvas().style.cursor = '';
+        });
     });
-
-
-    // Creation de la barre de recherche
-    map.addControl(new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
-    }));
-
-    //Template de map différente
-    var layerList = document.getElementById('menu');
-    var inputs = layerList.getElementsByTagName('input');
-
-    function switchLayer(layer) {
-        var layerId = layer.target.id;
-        map.setStyle('mapbox://styles/mapbox/' + layerId);
-    }
-
-    for (var i = 0; i < inputs.length; i++) {
-        inputs[i].onclick = switchLayer;
-    }
-
-    //Geolocalisation
-    map.addControl(new mapboxgl.GeolocateControl({
-        positionOptions: {
-            enableHighAccuracy: true
-        },
-        trackUserLocation: true
-    }));
-
-    //Ajout du navigation controle
-    map.addControl(new mapboxgl.NavigationControl());
 
 </script>
 <?php get_footer(); ?>
