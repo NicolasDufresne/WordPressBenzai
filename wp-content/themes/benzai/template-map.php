831c8560a92c64a482cb857377ff71e27275aa10
Angot <bat76640@gmail.com>
2020-01-07 14:25:47 +0100

    Add popup to marker

--- wp-content/themes/benzai/template-map.php
+++ wp-content/themes/benzai/template-map.php
@@ -109,22 +109,33 @@
                     }
                 });
             });
-        });
+            // inspect a cluster on click
+            map.on('click', 'clusters', function (e) {
+                var features = map.queryRenderedFeatures(e.point, {layers: ['clusters']});
+                var clusterId = features[0].properties.cluster_id;
+                map.getSource('BIN').getClusterExpansionZoom(clusterId, function (err, zoom) {
+                    if (err)
+                        return;
 
-        // inspect a cluster on click
-        map.on('click', 'clusters', function (e) {
-            var features = map.queryRenderedFeatures(e.point, {layers: ['clusters']});
-            var clusterId = features[0].properties.cluster_id;
-            map.getSource('BIN').getClusterExpansionZoom(clusterId, function (err, zoom) {
-                if (err)
-                    return;
-
-                map.easeTo({
-                    center: features[0].geometry.coordinates,
-                    zoom: zoom
+                    map.easeTo({
+                        center: features[0].geometry.coordinates,
+                        zoom: zoom
+                    });
                 });
             });
+
+            map.on('click','unclustered-point',function (e) {
+                var description = e.features[0].properties.commune;
+                var coordinates = e.features[0].geometry.coordinates.slice();
+
+                new mapboxgl.Popup()
+                    .setLngLat(coordinates)
+                    .setHTML(description)
+                    .addTo(map);
+            });
         });
+
+
 
         map.on('mouseenter', 'clusters', function () {
             map.getCanvas().style.cursor = 'pointer';
