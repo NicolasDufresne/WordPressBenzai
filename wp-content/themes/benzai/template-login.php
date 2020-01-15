be3c2ddd6917196f583ecfa403efda68a4d5d56f
Nicolas Dufresne <17339@csmrouen.net>
2020-01-15 10:22:47 +0100

    Login now working

--- wp-content/themes/benzai/template-login.php
+++ wp-content/themes/benzai/template-login.php
@@ -1,19 +1,25 @@
 <?php /* Template Name: Login */ ?>
 
 <?php
-if (!empty($_POST['submitted'])) {
-    $creds = array();
-    $creds['user_login'] = $_POST['login'];
-    $creds['user_password'] = $_POST['password'];
-    $creds['remember'] = true;
 
-    $user = wp_signon( $creds, false );
-
-    if ( is_wp_error($user) ) {
-        echo $user->get_error_message();
+if (isset($_POST['submitted'])):
+    $login_data = array();
+    $login_data['user_login'] = sanitize_user($_POST['login']);
+    $login_data['user_password'] = esc_attr($_POST['password']);
+    $user_data = wp_signon($login_data, false);
+    if (is_wp_error($user_data)) {
+        echo '<script>alert("T\'es pas co")</script>';
+    } else {
+        wp_clear_auth_cookie();
+        do_action('wp_login', $user_data->ID);
+        wp_set_current_user($user_data->ID);
+        wp_set_auth_cookie($user_data->ID, true);
+        $redirect_to = esc_url(home_url('/'));
+        wp_safe_redirect($redirect_to);
+        echo '<script>alert("T\'es co")</script>';
+        exit;
     }
-    print_r($_SESSION);
-}
+endif;
 ?>
 
 <?php get_header(); ?>
