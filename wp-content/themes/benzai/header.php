fda7f3fd27d90f89bd394a222f48273d2dd3e8fc
Nicolas Dufresne <17339@csmrouen.net>
2020-01-17 16:55:22 +0100

    work in progress

--- wp-content/themes/benzai/header.php
+++ wp-content/themes/benzai/header.php
@@ -10,40 +10,40 @@
  */
 
 ?>
-<!DOCTYPE html>
+    <!DOCTYPE html>
 
 <html <?php language_attributes(); ?>>
 
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Benzai</title>
-    <meta name="description" content="">
-    <link href="https://fonts.googleapis.com/css?family=Arvo:400,700" rel="stylesheet">
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Benzai</title>
+        <meta name="description" content="">
+        <link href="https://fonts.googleapis.com/css?family=Arvo:400,700" rel="stylesheet">
 
-    <!-- Jquery pour la navbar -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="profile" href="https://gmpg.org/xfn/11">
-    <script src='https://api.mapbox.com/mapbox-gl-js/v1.4.1/mapbox-gl.js'></script>
-    <link href='https://api.mapbox.com/mapbox-gl-js/v1.4.1/mapbox-gl.css' rel='stylesheet'/>
-    <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.2/mapbox-gl-geocoder.min.js'></script>
-    <link rel='stylesheet'
-          href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.2/mapbox-gl-geocoder.css'
-          type='text/css'/>
+        <!-- Jquery pour la navbar -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel="profile" href="https://gmpg.org/xfn/11">
+        <script src='https://api.mapbox.com/mapbox-gl-js/v1.4.1/mapbox-gl.js'></script>
+        <link href='https://api.mapbox.com/mapbox-gl-js/v1.4.1/mapbox-gl.css' rel='stylesheet'/>
+        <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.2/mapbox-gl-geocoder.min.js'></script>
+        <link rel='stylesheet'
+              href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.2/mapbox-gl-geocoder.css'
+              type='text/css'/>
 
-    <!-- Promise polyfill script required to use Mapbox GL Geocoder in IE 11 -->
-    <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
+        <!-- Promise polyfill script required to use Mapbox GL Geocoder in IE 11 -->
+        <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
 
-    <!-- Favicon -->
-    <link rel="icon" href="<?= get_template_directory_uri() . '/assets/img/benzai_bouteille.svg'; ?>" />
+        <!-- Favicon -->
+        <link rel="icon" href="<?= get_template_directory_uri() . '/assets/img/benzai_bouteille.svg'; ?>"/>
 
-    <?php wp_head(); ?>
+        <?php wp_head(); ?>
 
-    <!-- WOW.JS -->
-    <script>new WOW().init();</script>
-</head>
+        <!-- WOW.JS -->
+        <script>new WOW().init();</script>
+    </head>
 
 <body <?php body_class(); ?>>
 
@@ -63,6 +63,7 @@
         </div>
         <div id="mainListDiv" class="main_list">
             <ul class="navlinks">
+                <li><a href="<?php echo esc_url(home_url('disconnect')); ?>">Disconnect</a></li>
                 <li><a href="<?php echo esc_url(home_url('login')); ?>">Connexion</a></li>
                 <li><a href="http://localhost/WordPressBenzaiTheme/#about">À propos</a></li>
                 <li><a href="http://localhost/WordPressBenzaiTheme/#benzai">Qu'est-ce que Benzai ?</a></li>
